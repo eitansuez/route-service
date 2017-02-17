a9d4e323e8385a9358a18912aaf4b5f924498177
Eitan Suez <eitan.suez@gmail.com>
2017-02-16 20:04:17 -0600

    mostly whitespace; removed errant semicolon; simplified return logic in ratelimiter

--- src/main/java/org/cloudfoundry/example/Controller.java
+++ src/main/java/org/cloudfoundry/example/Controller.java
@@ -16,8 +16,6 @@
 
 package org.cloudfoundry.example;
 
-import java.net.URI;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,50 +27,50 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestOperations;
 
+import java.net.URI;
+
 
 @RestController
 final class Controller {
 
-    static final String FORWARDED_URL = "X-CF-Forwarded-Url";
+  static final String FORWARDED_URL = "X-CF-Forwarded-Url";
+  static final String PROXY_METADATA = "X-CF-Proxy-Metadata";
+  static final String PROXY_SIGNATURE = "X-CF-Proxy-Signature";
 
-    static final String PROXY_METADATA = "X-CF-Proxy-Metadata";
+  private final static Logger logger = LoggerFactory.getLogger(Controller.class);
 
-    static final String PROXY_SIGNATURE = "X-CF-Proxy-Signature";
+  private final RestOperations restOperations;
+  private RateLimiter rateLimiter;
 
-    private final static  Logger logger = LoggerFactory.getLogger(Controller.class);
+  @Autowired
+  Controller(RestOperations restOperations, RateLimiter rateLimiter) {
+    this.restOperations = restOperations;
+    this.rateLimiter = rateLimiter;
+  }
 
-    private final RestOperations restOperations;
-
-  	private RateLimiter rateLimiter;
-
-    @Autowired
-    Controller(RestOperations restOperations,  RateLimiter rateLimiter) {
-        this.restOperations = restOperations;
-        this.rateLimiter = rateLimiter;
+  @RequestMapping(headers = {FORWARDED_URL, PROXY_METADATA, PROXY_SIGNATURE})
+  ResponseEntity<?> service(RequestEntity<byte[]> incoming) {
+    logger.debug("Incoming Request: {}", incoming);
+    if (rateLimiter.rateLimitRequest(incoming)) {
+      logger.debug("Rate Limit imposed");
+      return new ResponseEntity<>(HttpStatus.TOO_MANY_REQUESTS);
     }
 
-    @RequestMapping(headers = {FORWARDED_URL, PROXY_METADATA, PROXY_SIGNATURE})
-    ResponseEntity<?> service(RequestEntity<byte[]> incoming) {
-        logger.debug("Incoming Request: {}", incoming);
-        if(rateLimiter.rateLimitRequest(incoming)){
-            logger.debug("Rate Limit imposed");
-        	return new ResponseEntity<>(HttpStatus.TOO_MANY_REQUESTS);
-        };
-        RequestEntity<?> outgoing = getOutgoingRequest(incoming);
-        logger.debug("Outgoing Request: {}", outgoing);
+    RequestEntity<?> outgoing = getOutgoingRequest(incoming);
+    logger.debug("Outgoing Request: {}", outgoing);
 
-        return this.restOperations.exchange(outgoing, byte[].class);
-    }
+    return this.restOperations.exchange(outgoing, byte[].class);
+  }
 
-    private static RequestEntity<?> getOutgoingRequest(RequestEntity<?> incoming) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.putAll(incoming.getHeaders());
-        URI uri = headers.remove(FORWARDED_URL).stream()
-            .findFirst()
-            .map(URI::create)
-            .orElseThrow(() -> new IllegalStateException(String.format("No %s header present", FORWARDED_URL)));
+  private static RequestEntity<?> getOutgoingRequest(RequestEntity<?> incoming) {
+    HttpHeaders headers = new HttpHeaders();
+    headers.putAll(incoming.getHeaders());
+    URI uri = headers.remove(FORWARDED_URL).stream()
+        .findFirst()
+        .map(URI::create)
+        .orElseThrow(() -> new IllegalStateException(String.format("No %s header present", FORWARDED_URL)));
 
-        return new RequestEntity<>(incoming.getBody(), headers, incoming.getMethod(), uri);
-    }
+    return new RequestEntity<>(incoming.getBody(), headers, incoming.getMethod(), uri);
+  }
 
 }
